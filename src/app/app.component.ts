c44eae71a7e99c2a85ffd66f767a6b002a000b60
lisp719 <368034+lisp719@users.noreply.github.com>
2022-11-23 02:02:07 +0900

    feat: Implement to change strength of the prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  prompts: { text: string }[] = [];
+  rawText = '';
+
+  copyToClipboard() {
+    if (this.prompts.length > 0) {
+      const text = this.prompts.map((p) => p.text).join(', ');
+
+      navigator.clipboard.writeText(`${text}, `);
+    }
+  }
+
+  decrement(prompt: { text: string }) {
+    if (/\{|\}/.test(prompt.text)) {
+      prompt.text = prompt.text.replace('{', '').replace('}', '');
+    } else {
+      prompt.text = `[${prompt.text}]`;
+    }
+  }
+
+  increment(prompt: { text: string }) {
+    if (/\[|\]/.test(prompt.text)) {
+      prompt.text = prompt.text.replace('[', '').replace(']', '');
+    } else {
+      prompt.text = `{${prompt.text}}`;
+    }
+  }
+
+  onSubmit() {
+    this.prompts = this.rawText
+      .split(',')
+      .map((t) => t.trim())
+      .filter((t) => t)
+      .map((t) => ({ text: t }));
+  }
+
+  remove(prompt: { text: string }) {
+    this.prompts = this.prompts.filter((p) => p !== prompt);
+  }
+}
