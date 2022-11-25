86c38ebe239fb0088614f3c8b4991515e1deeb5f
lisp719 <368034+lisp719@users.noreply.github.com>
2022-11-26 04:48:14 +0900

    feat: Automatically apply prompt changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,13 @@ export class AppComponent implements OnInit {
     this.readFromPage();
   }
 
-  copyToClipboard() {
-    if (this.prompts.length > 0) {
-      const text = this.prompts.map((p) => p.text).join(', ');
-
-      navigator.clipboard.writeText(`${text}, `);
-    }
-  }
-
   decrement(prompt: { text: string }) {
     if (/\{|\}/.test(prompt.text)) {
       prompt.text = prompt.text.replace('{', '').replace('}', '');
     } else {
       prompt.text = `[${prompt.text}]`;
     }
+    this.updateInput();
   }
 
   increment(prompt: { text: string }) {
@@ -35,10 +28,12 @@ export class AppComponent implements OnInit {
     } else {
       prompt.text = `{${prompt.text}}`;
     }
+    this.updateInput();
   }
 
   movePrompt(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.prompts, event.previousIndex, event.currentIndex);
+    this.updateInput();
   }
 
   async readFromPage() {
@@ -74,5 +69,35 @@ export class AppComponent implements OnInit {
 
   remove(prompt: { text: string }) {
     this.prompts = this.prompts.filter((p) => p !== prompt);
+    this.updateInput();
+  }
+
+  async updateInput() {
+    if (this.prompts.length) {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      const text = this.prompts.map((p) => p.text).join(',') + ', ';
+
+      if (tab.id) {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          args: [text],
+          func: (text: string) => {
+            const dom =
+              document.querySelector<HTMLInputElement>('#prompt-input-0');
+
+            if (dom) {
+              dom.value = text;
+              // @ts-expect-error NOTE: clear the interal value to force an actual change
+              dom._valueTracker?.setValue('');
+              dom.dispatchEvent(new Event('input', { bubbles: true }));
+            }
+          },
+        });
+      }
+    }
   }
 }
